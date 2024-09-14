473d69053c7eb189698907a1c0e6d9bf33e7cfc8
Bilal Syed <bsyed7633@conestogac.on.ca>
2024-09-13 20:40:25 -0400

    changed some file names. added header file for function definitions. commented the general layout of what will be happening in each file.

---
+++ InventoryMgmtSys.c
@@ -0,0 +1,32 @@
+#include "InventoryMgmtSys.h"
+
+int main(void)
+{
+	/*GENERAL LAYOOUT OF MAIN:
+	* 
+	- menu showing options for which kind of file 
+	the user can perform the CRUD operations on
+
+	- get user input for their choice from the menu
+
+	- use switch-case statement for the different menu options 
+	that the user can choose
+
+	- they have three options: 1. random accsess, 2. sequential access, 3. delimited file
+	
+	- once they make a selection another menu will show up. It will have 4 different options for CRUD.
+	the same CRUD menu will show up no matter which file type is chosen
+	
+	- another switch-case statement for the CRUD menu options
+	
+	- this is where the CRUD functions will be called. each file type will have its own dedicated CRUD functions. 
+	create function, read function, update function, and delete function for each respective file type.
+	this means that each file type will have, at minimum, 4 different functions
+	
+	- for create and update let the user input what new values they want to add. for delete let them decide which entry 
+	they would like to delete.
+	*
+	*/
+
+	return 0;
+}
---
+++ InventoryMgmtSys.h
@@ -0,0 +1,57 @@
+#pragma once
+
+
+#ifndef INVENTORY_MGMT_SYS_H
+#define INVENTORY_MGMT_SYS_H 
+
+#pragma warning(disable : 4996)
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//THIS FILE IS WHERE ALL OF THE FUNCTION PROTOTYPES WILL GO 
+
+//Random Access
+/*
+- function to create an entry
+
+- function to read out entries
+
+- functiom to update an entry
+
+- function to delete entries
+
+- any other functions necessary...
+*/
+
+//Sequential Access
+/*
+- function to create an entry
+
+- function to read out entries
+
+- functiom to update an entry
+
+- function to delete entries
+
+- any other functions necessary...
+*/
+
+//Delimited File
+/*
+- function to create an entry
+
+- function to read out entries
+
+- functiom to update an entry
+
+- function to delete entries
+
+- any other functions necessary...
+*/
+
+
+#endif
+
+

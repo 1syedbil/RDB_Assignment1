662fd7682a7aa5983c308131a4f48a7ebd09f9a9
MorganTabor <ctabor5298@conestogac.on.ca>
2024-09-18 00:15:07 -0400

    Added boolean functionality, began work on a function that creates a file, and a function that will write to it. This function will also validate user inputs when called, eventually. I haven't got there yet.

--- DelimitedFile.c
+++ DelimitedFile.c
@@ -2,3 +2,41 @@
 
 
 //THIS IS WHERE THE DEFINITIONS FOR ALL OF THE FUNCTIONS RELATED TO DELIMITED FILE WILL GO
+
+const char kDelimFile[8] = "FileCSV";
+
+/// <summary>
+/// Creates a text file named "FileCSV"
+/// </summary>
+/// <returns>Pointer to created file</returns>
+FILE* fileStarter() 
+{
+	FILE *delimFile = NULL;
+	delimFile = fopen(kDelimFile, "rw");
+	
+	//Checks if file opened correctly
+	if (delimFile = NULL) 
+	{
+		printf("File unable to open\n");
+	}
+	else
+	{
+		printf("File opened successfully.\n");
+	}
+
+	fclose(delimFile);
+	return delimFile;
+}
+
+/// <summary>
+/// Opens FileCSV in write-only mode to write to.
+/// </summary>
+/// <param name="pointer"> </param>
+/// <returns></returns>
+FILE* writeToFile(FILE* pointer) 
+{
+	FILE* delimFile = pointer;
+	delimFile = fopen("FileCSV", "w");
+
+
+}

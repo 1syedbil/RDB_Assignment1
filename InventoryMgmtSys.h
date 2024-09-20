20f537de27a68befffeff0e34be8c532bab8fe2d
QMinh286 <147283856+QMinh286@users.noreply.github.com>
2024-09-20 01:13:05 -0400

    Update the structure and function prototype for sequential access

--- InventoryMgmtSys.h
+++ InventoryMgmtSys.h
@@ -4,6 +4,7 @@
 #ifndef INVENTORY_MGMT_SYS_H
 #define INVENTORY_MGMT_SYS_H 
 
+#define MAX_PRODUCTS 120
 #pragma warning(disable : 4996)
 
 #include <stdio.h>
@@ -28,15 +29,26 @@
 
 //Sequential Access
 /*
-- function to create an entry
 
-- function to read out entries
 
-- functiom to update an entry
 
-- function to delete entries
+//STRUCTURE TO HOLD DATA
+struct Product {
+	int id;
+	char name[50];
+	char category[20];
+	int quantity;
+	float price;
+}Product;
 
-- any other functions necessary...
+ //FUNCTION PROTOTYPE
+int ReadProducts(struct Product products[]);
+void WriteProducts(struct Product products[], int times);
+void AddProduct(struct Product products[], int* times);
+void UpdateProduct(struct Product products[], int times);
+void DeleteProduct(struct Product products[], int* times);
+void DisplayProducts(struct Product products[], int times);
+
 */
 
 //Delimited File

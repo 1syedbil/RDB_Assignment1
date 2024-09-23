47dc528fc8b8adec4aabce95a805ab477aa19aac
1syedbil <153937103+1syedbil@users.noreply.github.com>
2024-09-23 01:45:31 -0400

    Committed most recent changes from BilalBranch_New to master branch.

--- InventoryMgmtSys.h
+++ InventoryMgmtSys.h
@@ -1,10 +1,8 @@
 #pragma once
 
-
-#ifndef INVENTORY_MGMT_SYS_H
+#ifndef INVENTORY_MGMT_SYS_H 
 #define INVENTORY_MGMT_SYS_H 
 
-#define MAX_PRODUCTS 120
 #pragma warning(disable : 4996)
 
 #include <stdio.h>
@@ -12,44 +10,55 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+
+//CONSTANT DEFINITIONS
+#define STRING_LEN 50
+#define MAX_PRODUCTS 120
+
+//STRUCT DEFINITIONS
+
+//for random access
+typedef struct 
+{
+    int productId;
+    char name[STRING_LEN];
+    char category[STRING_LEN];
+    int quantity;
+    double price;
+} RndAccessRecord;
+
+//for sequential access
+typedef struct
+{
+	int id;
+	char name[STRING_LEN];
+	char category[STRING_LEN];
+	int quantity;
+	double price;
+}Product;
+
+bool validateString(char input[STRING_LEN]);
+bool validateInt(char input[STRING_LEN]);
+bool validateDouble(char input[STRING_LEN]);
+char* getUserInput(char input[STRING_LEN]); 
+
 //THIS FILE IS WHERE ALL OF THE FUNCTION PROTOTYPES WILL GO 
 
 //Random Access
-/*
-- function to create an entry
 
-- function to read out entries
-
-- functiom to update an entry
-
-- function to delete entries
-
-- any other functions necessary...
-*/
+void rndAccessCreateRecord(char fileName[STRING_LEN]);
+void rndAccessReadRec(char fileName[STRING_LEN]);
+void rndAccessUpdtRec(char fileName[STRING_LEN]);
+void rndAccessDltRecord(char fileName[STRING_LEN]);
+int checkRecExists(char fileName[STRING_LEN], int id);
+long calculateOffset(char fileName[STRING_LEN], int id); 
 
 //Sequential Access
 
-
-
-
-//STRUCTURE TO HOLD DATA
-struct Product {
-	int id;
-	char name[50];
-	char category[20];
-	int quantity;
-	float price;
-}Product;
-
- //FUNCTION PROTOTYPE
-int ReadProducts(struct Product products[]);
-void WriteProducts(struct Product products[], int times);
-void AddProduct(struct Product products[], int* times);
-void UpdateProduct(struct Product products[], int times);
-void DeleteProduct(struct Product products[], int* times);
-void DisplayProducts(struct Product products[], int times);
-
-
+void addProduct(char fileName[STRING_LEN]);
+void displayProducts(char fileName[STRING_LEN]);
+void updateProduct(char fileName[STRING_LEN]);
+void deleteProduct(char fileName[STRING_LEN]);
 
 //Delimited File
 /*
@@ -66,5 +75,3 @@ void DisplayProducts(struct Product products[], int times);
 
 
 #endif
-
-

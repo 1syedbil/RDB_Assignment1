b8410eebab1868a9b589feec83644addb59c6e60
1syedbil <153937103+1syedbil@users.noreply.github.com>
2024-09-23 01:43:34 -0400

    Committed most recent changes from BilalBranch_New to master branch.

--- InventoryMgmtSys.c
+++ InventoryMgmtSys.c
@@ -2,31 +2,166 @@
 
 int main(void)
 {
-	/*GENERAL LAYOOUT OF MAIN:
-	* 
-	- menu showing options for which kind of file 
-	the user can perform the CRUD operations on
+	char seqFileName[STRING_LEN] = "sequentialFile.txt";
+	char rndFileName[STRING_LEN] = "randomFile.bin";
+	char userInput[STRING_LEN] = "";
+	int choice1 = 0;
+	int choice2 = 0;
 
-	- get user input for their choice from the menu
+	do
+	{
+		printf("File Access Program Menu:\n\n     1. Sequential Access     2. Random Access     3. Delimited File     4. Exit\n\n");
+		printf("Enter a number corresponding with your menu selection: ");
+		strcpy(userInput, getUserInput(userInput));   
+		printf("\n\n");   
+		if (!validateInt(userInput))
+		{
+			continue;
+		}
+		choice1 = atoi(userInput);  
 
-	- use switch-case statement for the different menu options 
-	that the user can choose
+		switch (choice1) 
+		{
+		case 1:
+			do
+			{
 
-	- they have three options: 1. random accsess, 2. sequential access, 3. delimited file
-	
-	- once they make a selection another menu will show up. It will have 4 different options for CRUD.
-	the same CRUD menu will show up no matter which file type is chosen
-	
-	- another switch-case statement for the CRUD menu options
-	
-	- this is where the CRUD functions will be called. each file type will have its own dedicated CRUD functions. 
-	create function, read function, update function, and delete function for each respective file type.
-	this means that each file type will have, at minimum, 4 different functions
-	
-	- for create and update let the user input what new values they want to add. for delete let them decide which entry 
-	they would like to delete.
-	*
-	*/
+				printf("Sequential Access Menu:\n\n     1. Create a Record     2. Read All Records     3. Update a Record     4. Delete a Record     5. Back\n\n"); 
+				printf("Enter a number corresponding with your menu selection: "); 
+				strcpy(userInput, getUserInput(userInput)); 
+				printf("\n\n");
+				if (!validateInt(userInput))
+				{
+					continue;
+				}
+				choice2 = atoi(userInput); 
+
+				switch (choice2) 
+				{ 
+				case 1:
+					addProduct(seqFileName); 
+					break;
+
+				case 2:
+					displayProducts(seqFileName); 
+					break;
+
+				case 3:
+					updateProduct(seqFileName);
+					break;
+
+				case 4:
+					deleteProduct(seqFileName); 
+					break;
+
+				case 5:
+					printf("Going back...\n\n"); 
+					break;
+
+				default:
+					printf("Error: invalid menu selection.\n\n");
+					break;
+				}
+
+			} while (choice2 != 5);
+
+			break;
+
+		case 2:
+			do
+			{
+				printf("Random Access Menu:\n\n     1. Create a Record     2. Read a Record     3. Update a Record     4. Delete a Record     5. Back\n\n");
+				printf("Enter a number corresponding with your menu selection: ");
+				strcpy(userInput, getUserInput(userInput));
+				printf("\n\n");
+				if (!validateInt(userInput))
+				{
+					continue;
+				}
+				choice2 = atoi(userInput);
+
+				switch (choice2)
+				{
+				case 1:
+					rndAccessCreateRecord(rndFileName);
+					break;
+
+				case 2:
+					rndAccessReadRec(rndFileName);
+					break;
+
+				case 3:
+					rndAccessUpdtRec(rndFileName);
+					break;
+
+				case 4:
+					rndAccessDltRecord(rndFileName);
+					break;
+
+				case 5:
+					printf("Going back...\n\n");
+					break;
+
+				default:
+					printf("Error: invalid menu selection.\n\n");
+					break;
+				}
+
+			} while (choice2 != 5);
+
+			break;
+
+		case 3:
+			do
+			{
+
+				printf("Delimited File Menu:\n\n     1. Create a Record     2. Read All Records     3. Update a Record     4. Delete a Record     5. Back\n\n");
+				printf("Enter a number corresponding with your menu selection: ");
+				strcpy(userInput, getUserInput(userInput));
+				printf("\n\n");  
+				choice2 = atoi(userInput); 
+
+				switch (choice2)
+				{
+				case 1:
+
+					break;
+
+				case 2:
+
+					break;
+
+				case 3:
+
+					break;
+
+				case 4:
+
+					break;
+
+				case 5:
+					printf("Going back...\n\n");
+					break;
+
+				default:
+					printf("Error: invalid menu selection.\n\n");
+					break;
+				}
+
+			} while (choice2 != 5);
+
+			break;
+
+		case 4:
+			printf("Exiting...");
+			break;
+
+		default:
+			printf("Error: invalid menu selection.\n\n");
+			break;
+		}
+
+	} while (choice1 != 4);
 
 	return 0;
 }
